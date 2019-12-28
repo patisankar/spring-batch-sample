21ab4c08f5b119f6c298e5e01bb3ce216afd3575
sample <sankar.pati1@gmail.com>
2019-12-28 09:51:42 -0600

    initial commit

---
+++ src/main/java/com/batch/stat/StatApplication.java
@@ -0,0 +1,13 @@
+package com.batch.stat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatApplication.class, args);
+	}
+
+}
